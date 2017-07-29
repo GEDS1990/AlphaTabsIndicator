5716ddb4a0714d24128f637ed3f7cc6b9853bde3
GEDS1990 <1161865398@qq.com>
2017-07-29 22:29:19 +0800

    commit by gds @20170729

--- app/src/main/java/com/yinglan/alphatabsindicator/MainActivity.java
+++ app/src/main/java/com/yinglan/alphatabsindicator/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.yinglan.alphatabs.AlphaTabsIndicator;
 
 import java.util.ArrayList;
@@ -15,12 +17,19 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private AlphaTabsIndicator alphaTabsIndicator;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        webView = (WebView) findViewById(R.id.webView);
+        //WebView加载web资源
+        webView.loadUrl("http://www.haveideal.club");
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient(){
+        });
+/*
         ViewPager mViewPger = (ViewPager) findViewById(R.id.mViewPager);
         MainAdapter mainAdapter = new MainAdapter(getSupportFragmentManager());
         mViewPger.setAdapter(mainAdapter);
@@ -32,14 +41,14 @@ public class MainActivity extends AppCompatActivity {
         alphaTabsIndicator.getTabView(0).showNumber(6);
         alphaTabsIndicator.getTabView(1).showNumber(888);
         alphaTabsIndicator.getTabView(2).showNumber(88);
-        alphaTabsIndicator.getTabView(3).showPoint();
+        alphaTabsIndicator.getTabView(3).showPoint();*/
     }
 
 
     private class MainAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener {
 
         private List<Fragment> fragments = new ArrayList<>();
-        private String[] titles = {"微信", "通讯录", "发现", "我"};
+        private String[] titles = {"放贷", "贷款", "发现", "我"};
 
         public MainAdapter(FragmentManager fm) {
             super(fm);

122fab678c23765a622a9794406282fee699b564
GEDS1990 <1161865398@qq.com>
2017-07-30 18:20:57 +0800

    commit by gds @20170729

--- app/src/main/java/com/yinglan/alphatabsindicator/MainActivity.java
+++ app/src/main/java/com/yinglan/alphatabsindicator/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import com.yinglan.alphatabs.AlphaTabsIndicator;
@@ -19,15 +20,25 @@ public class MainActivity extends AppCompatActivity {
     private AlphaTabsIndicator alphaTabsIndicator;
     private WebView webView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webView);
+        webView = (WebView) findViewById(R.id.webView);//启用支持javascript
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
         //WebView加载web资源
         webView.loadUrl("http://www.haveideal.club");
         //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
         webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
         });
 /*
         ViewPager mViewPger = (ViewPager) findViewById(R.id.mViewPager);
